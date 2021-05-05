70322580f7f4ec25af9a4ad0b39a66cd76e4f519
JhaMurlidhar <murli555@gmail.com>
2021-05-05 19:33:22 +0530

    Prepare test data and implement test scenario

---
+++ CheckoutPromotion/CheckoutPromotionUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CheckoutPromotionUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CheckoutPromotionUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cd0fa057-7a6c-48dc-ac03-533029536912")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CheckoutPromotion/CheckoutPromotionUnitTest/UnitTest1.cs
@@ -0,0 +1,88 @@
+﻿using CheckoutPromotionClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CheckoutPromotionUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static readonly IEnumerable<SKU_Price> PriceList = new List<SKU_Price>
+        {
+            new SKU_Price { SKU_Id = 'A', UnitPrice = 50 },
+            new SKU_Price { SKU_Id = 'B', UnitPrice = 30 },
+            new SKU_Price { SKU_Id = 'C', UnitPrice = 20 },
+            new SKU_Price { SKU_Id = 'D', UnitPrice = 15 }
+        };
+
+        static readonly IEnumerable<Promotion> Promotions =
+            new List<Promotion> {
+                new Promotion {
+                    Items = new List<Item> {
+                        new Item { SKU_Id = 'A', Quantity = 3 }},
+                    TotalAmount = 130 }, // 3A's for 130
+                new Promotion {
+                    Items = new List<Item> {
+                        new Item { SKU_Id = 'B', Quantity = 2 }},
+                    TotalAmount = 45 }, // 2B's for 45
+                new Promotion {
+                    Items = new List<Item> {
+                        new Item { SKU_Id = 'C', Quantity = 1 },
+                        new Item { SKU_Id = 'D', Quantity = 1 }},
+                    TotalAmount = 30 } // C + D for 30
+            };
+        static readonly DriverEngine objEngine = new DriverEngine(PriceList, Promotions);
+
+        [TestMethod]
+        public void Test_Case_A()
+        {
+            var order =
+                new Order
+                {
+                    Items = new List<Item>
+                    {
+                        new Item { SKU_Id = 'A', Quantity = 1 },
+                        new Item { SKU_Id = 'B', Quantity = 1 },
+                        new Item { SKU_Id = 'C', Quantity = 1 }
+                    }          
+                };
+            objEngine.CheckOut(order);
+            Assert.IsTrue(order.TotalAmount == 100);
+        }
+
+        [TestMethod]
+        public void Test_Case_B()
+        {
+            var order =
+                new Order
+                {
+                    Items = new List<Item>
+                    {
+                        new Item { SKU_Id = 'A', Quantity = 5 },
+                        new Item { SKU_Id = 'B', Quantity = 5 },
+                        new Item { SKU_Id = 'C', Quantity = 1 }
+                    }
+                };
+            objEngine.CheckOut(order);
+            Assert.IsTrue(order.TotalAmount == 370);
+        }
+
+        [TestMethod]
+        public void Test_Case_C()
+        {
+            var order =
+               new Order
+               {
+                   Items = new List<Item>
+                   {
+                        new Item { SKU_Id = 'A', Quantity = 3 },
+                        new Item { SKU_Id = 'B', Quantity = 5 },
+                        new Item { SKU_Id = 'C', Quantity = 1 },
+                        new Item { SKU_Id = 'D', Quantity = 1 }
+                   }
+               };
+            objEngine.CheckOut(order);
+            Assert.IsTrue(order.TotalAmount == 280);
+        }
+    }
+}
